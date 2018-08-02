aae1b58b5d8abc890a2365587c9b5100f312491e
Taylor Williams <taylor@thewilliams.net>
2018-08-02 03:50:02 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,7 +1,7 @@
 class Application
   def call(env)
     resp = Rack::Response.new
-    if DateTime.now.strftime("%H").to_i < 12
+    if time.to_i < 12
       resp.write "Good Morning!"
     else
       resp.write "Good Afternoon!"
